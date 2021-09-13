ff9ad825a0ab093463eb0eeea8cf8db03d27834e
YesicaAcha <yesica.7887@gmail.com>
2021-09-13 15:07:17 -0400

    api project

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="truck_delivery_yess",
+    version="0.0.1",
+    author="Yesica_Acha",
+    description="An application to manage trucks",
+    install_requires=['flask']
+)
