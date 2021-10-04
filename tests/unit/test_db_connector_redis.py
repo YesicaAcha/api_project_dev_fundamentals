f68475301ec5b4b50e77dfe88dff9fc644717c9a
YesicaAcha <yesica.7887@gmail.com>
2021-10-04 09:51:50 -0400

    fix test

--- tests/unit/test_db_connector_redis.py
+++ tests/unit/test_db_connector_redis.py
@@ -7,11 +7,15 @@ from truck_delivery_yess.db_connector.db_connector_redis import DBConnectorRedis
 
 @pytest.fixture
 def db_connector(mocker):
+    id_test = "123"
     object_to_save = {"123": {"a": "a"}}
+
+    second_id_test = "456"
     second_object_to_save = {"456": {"b": "b"}}
 
     mocker.patch("redis.Redis.set").return_value = True
     mocker.patch("redis.Redis.get").return_value = json.dumps(object_to_save)
+    mocker.patch("redis.Redis.scan_iter").return_value = [id_test, second_id_test]
     mocker.patch("redis.Redis.mget").return_value = [json.dumps(obj) for obj in [object_to_save, second_object_to_save]]
     mocker.patch("redis.Redis.delete").return_value = 1
 
